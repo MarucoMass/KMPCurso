bcb15bb9d601623e60dad00bf067bcdacd3473da
MarucoMass <mariomass23@gmail.com>
2025-04-08 23:25:52 -0300

    added more jetpack composables

--- androidApp/src/main/java/tech/mariomass/kmpcurso/android/MainActivity.kt
+++ androidApp/src/main/java/tech/mariomass/kmpcurso/android/MainActivity.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,22 +36,47 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            var isClicked by remember { mutableStateOf(false) }
+
             MyApplicationTheme {
+                /*
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .background(Color.Blue)
-                            .clickable {
-                                Log.d("Click", "hice click")
-                            }
-                            .padding(16.dp, 8.dp),
-                        text = "hola que tal"
-                    )
-                }
+                    */
+                    Card(
+                        modifier = Modifier.clickable {
+                            isClicked = !isClicked
+                            Log.d("Click", "$isClicked")
+                        },
+                        shape = RoundedCornerShape(36),
+                        elevation = CardDefaults.cardElevation(10.dp),
+                        border = BorderStroke(5.dp, Color.LightGray)
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.Top
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.baseline_person_24),
+                                contentDescription = "Profile"
+                            )
+                            GreetingView(
+                                modifier = Modifier
+                                    .wrapContentSize()
+                                    .background(Color.Blue)
+                                    .clickable {
+                                        Log.d("Click", "hice click")
+                                    }
+                                    .padding(16.dp, 8.dp),
+                                text = if (!isClicked) "Clicked" else "Click me"
+                            )
+
+                        }
+                    }
+               /* } */
             }
         }
     }
@@ -50,7 +87,7 @@ fun GreetingView(modifier: Modifier, text: String) {
     Text(
         modifier = modifier,
         text = text,
-        color = Color.Red,
+        color = Color.White,
         fontSize = 20.sp,
         fontWeight = FontWeight.Bold,
 
